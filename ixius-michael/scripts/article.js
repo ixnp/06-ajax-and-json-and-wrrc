eceeac7fc092ccdbf861f9c4f55f69d038c2273a
Your Name <you@example.com>
2017-12-14 20:02:31 -0800

    Reviewed and Commented complete

--- ixius-michael/scripts/article.js
+++ ixius-michael/scripts/article.js
@@ -9,19 +9,20 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
-// REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
+// REVIEWED: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
 // COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
+// Due to the usage of the "this" keyword. "This" should be bound to the instance of the function in which its used and using an arrow function would cause "this" to inherit to the parent scope.
+
 Article.prototype.toHtml = function() {
   let template = Handlebars.compile($('#article-template').text());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
-  // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
+  // COMMENTED: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
   // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  // This is ternary operator which can be used in place of an if/else statement.
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -33,28 +34,28 @@ Article.prototype.toHtml = function() {
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
 // COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+// rawData is called in the Article.fetchAll function. rawData represents an argument in this instance. in the past it held the article content. This time around it's passed as a argument and used as a keyword inside the Article.loadAll function.
 Article.loadAll = rawData => {
   rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
   rawData.forEach(articleObject => Article.all.push(new Article(articleObject)))
+
+  rawData = ['Monkey','Monkey2','Monkey3']
 }
+Article.loadAll();
+
 
 // REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
-console.log('stuff and things 1');
 Article.fetchAll = () => {
   // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
   if (localStorage.rawData) {
 
     Article.loadAll();
-console.log('stuff and things 2');
   } else { 
-    console.log('stuff and things 3');
     
     $.getJSON('./data/hackerIpsum.json').then(function(data){
       console.log('things', data);
     });
-
   }
 }
 
